27d8a006079e0d3f72d25e2d2c55689cf6c55e07
zhuyingda <383329144@qq.com>
2015-07-03 00:26:54 +0800

    1

--- src/pager/pager.js
+++ src/pager/pager.js
@@ -5,7 +5,17 @@ var React = require('react');
 
 var Pager = React.createClass({
   render : function () {
-    return <div>Hello {this.props.thick}</div>;
+    return (
+      <div class="z_pager_container">
+        <div class="z_pager_first">首页</div>
+        <div class="z_pager_prev">上一页</div>
+        <div class="z_pager_wrap">
+
+        </div>
+        <div class="z_pager_next">下一页</div>
+        <div class="z_pager_last">尾页</div>
+      </div>
+    );
   }
 });
 
